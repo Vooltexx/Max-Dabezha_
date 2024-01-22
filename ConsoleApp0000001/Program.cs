c638e4480d4cb81508fa2a80426b644d4c2e87b6
Vooltexx <152155025+Vooltexx@users.noreply.github.com>
2024-01-22 23:58:56 +0300

    Add files via upload

---
+++ ConsoleApp0000001/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0000001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1.Вывести на экран в столбик первые 10 натуральных чисел(циклы с условием, цикл с параметром)
+            //int x = 1;
+            //while (x <= 10)
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+
+            //int x = 1;
+            //do
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+            ////условие
+            //while (x <= 10);
+
+            //for (int x = 1; x <= 10; x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //Задание 2.Вывести на экран целые числа из интервала от 1 до n(цикл с параметром)
+            //Console.Write("Введите n:");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int x = 1; x <= n; x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //Задание 3.Вывести в столбик 5 раз слово «Привет!» (циклы с условием)
+            //for (int x = 1; x <= 5; x++)
+            //{
+            //    Console.WriteLine("Привет!");
+            //}
+
+
+            //Задание 4.С использованием цветового оформления консоли:
+            //// а) вывести на экран горизонтальную строку из 18 символов
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //for (int i = 0; i < 18; i++)
+            //{
+            //    Console.Write('0');
+            //}
+            //Console.ResetColor();
+            //Console.WriteLine();
+
+            //// б) вывести на экран вертикальную строку из 25 символов
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //for (int i = 1; i < 25; i++)
+            //{
+            //    Console.WriteLine('0');
+            //}
+            //Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
